35f910a0319274c15f454a95d12b7fce42d8b855
Abhinaya2902 <abhinaya.p@infomericainc.com>
2025-05-09 14:53:31 +0530

    Create StudentRestApplication.java

---
+++ src/main/java/com/example/Studentrest/StudentRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.Studentrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRestApplication.class, args);
+    }
+}
+
