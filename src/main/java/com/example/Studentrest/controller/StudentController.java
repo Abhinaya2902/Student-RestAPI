65a5030e016011e45178717e6af768c9165cb667
Abhinaya2902 <abhinaya.p@infomericainc.com>
2025-05-09 14:49:30 +0530

    Create Controller for Student Details

---
+++ src/main/java/com/example/Studentrest/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.Studentrest.controller;
+
+import com.example.Studentrest.dto.StudentDTO;
+import com.example.Studentrest.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/{all}")
+    public List<StudentDTO> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public StudentDTO createStudent(@RequestBody StudentDTO studentDTO) {
+        return studentService.createStudent(studentDTO);
+    }
+
+    @PutMapping("/{id}")
+    public StudentDTO updateStudent(@PathVariable Long id, @RequestBody StudentDTO studentDTO) {
+        return studentService.updateStudent(id, studentDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+}
