6c62974f2fbf32174df92f97df3313c7953d55a2
Abhinaya2902 <abhinaya.p@infomericainc.com>
2025-05-09 14:54:57 +0530

    Create StudentsdataApplicationTests.java

---
+++ src/test/java/com/example/Studentsdata/StudentsdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Studentsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
